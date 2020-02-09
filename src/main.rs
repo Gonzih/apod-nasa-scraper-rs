2df72ddda65c06860f765369113af38d0d03ee2c
Max Gonzih <gonzih@gmail.com>
2020-02-09 17:58:11 -0500

    Asyncafboi

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 #[macro_use]
 extern crate clap;
 
+use futures::future::join_all;
 use regex::Regex;
 use scraper::{Html, Selector};
 use std::path::Path;
+use std::sync::Arc;
 use tokio::fs::File;
 use tokio::prelude::*;
+use tokio::sync::Semaphore;
 
 const INDEX_URL: &'static str = "https://apod.nasa.gov/apod/archivepix.html";
 const ENTRY_PREFIX: &'static str = "https://apod.nasa.gov/apod/";
@@ -32,54 +35,70 @@ impl Entry {
         format!("{}{}", ENTRY_PREFIX, self.url)
     }
 
-    async fn download(&self, directory: &String) -> Result<(), Box<dyn std::error::Error>> {
+    async fn get_img_url(&self) -> Option<String> {
         let image_href_re = Regex::new(r"^image/.*\.jpg$").unwrap();
         let url = self.gen_url();
 
-        let index = reqwest::get(&url).await?.text().await?;
+        let index = reqwest::get(&url)
+            .await
+            .expect(&format!("Colud not get data from url {}", url))
+            .text()
+            .await
+            .expect(&format!("Colud not get response text from {}", url));
         let document = Html::parse_document(&index);
         let a_sel = Selector::parse("a").unwrap();
+
         for a in document.select(&a_sel) {
             if let Some(href) = a.value().attr("href") {
                 if image_href_re.is_match(href) {
-                    let url = format!("{}{}", ENTRY_PREFIX, href);
-                    self.download_file(url, directory).await?;
+                    return Some(format!("{}{}", ENTRY_PREFIX, href));
                 }
             }
         }
 
-        Ok(())
+        None
     }
 
     async fn download_file(
         &self,
-        url: String,
-        directory: &String,
+        directory: String,
+        sem: Arc<Semaphore>,
     ) -> Result<(), Box<dyn std::error::Error>> {
-        let extension_re = Regex::new(r"\.html$").unwrap();
-        let fname = format!(
-            "{} - {}.jpg",
-            extension_re.replace_all(&self.url, ""),
-            self.title
-        );
-        let p = Path::new(&directory).join(Path::new(&fname));
-        let path = &*p.to_string_lossy();
+        let _ = sem.acquire().await;
+        if let Some(url) = self.get_img_url().await {
+            let extension_re = Regex::new(r"\.html$").unwrap();
+            let fname = format!(
+                "{} - {}.jpg",
+                extension_re.replace_all(&self.url, ""),
+                self.title
+            );
+            let p = Path::new(&directory).join(Path::new(&fname));
+            let path = &*p.to_string_lossy();
 
-        if !p.exists() {
-            println!("Downloading file {} to {}", url, path);
+            if !p.exists() {
+                let _ = sem.acquire().await;
+                let response = reqwest::get(&*url).await?.bytes().await?;
 
-            let response = reqwest::get(&*url).await?.bytes().await?;
-            let mut dest = File::create(path).await?;
-            dest.write_all(&response).await?;
+                let _ = sem.acquire().await;
+                let mut dest = File::create(path).await?;
+
+                let _ = sem.acquire().await;
+                dest.write_all(&response).await?;
+            } else {
+                println!("Skipping file {}, file exists", path);
+            }
         } else {
-            println!("Skipping file {}, file exists", path);
+            println!(
+                "Skipping file {} - {}, could not load url",
+                self.url, self.title
+            );
         }
 
         Ok(())
     }
 }
 
-#[tokio::main]
+#[tokio::main(threaded_scheduler)]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opts: Opts = Opts::parse();
 
@@ -96,22 +115,32 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         for a in b.select(&a_sel) {
             if let Some(href) = a.value().attr("href") {
                 if index_href_re.is_match(href) {
-                    let text = a.text().collect::<String>();
-                    let entry = Entry::new(href.to_string(), text);
+                    let text = a.text().collect::<String>().clone();
+                    let href = href.to_string().clone();
+
+                    let entry = Entry::new(href, text);
                     entries.push(entry);
                 }
             }
         }
     }
 
-    let futures = entries
-        .iter()
-        .map(|e| e.download(&opts.directory))
-        .collect::<Vec<_>>();
+    let semaphore = Arc::new(Semaphore::new(10));
+    let mut handles = vec![];
 
-    for f in futures {
-        f.await?;
+    for entry in entries {
+        let directory = opts.directory.clone();
+        let sem = semaphore.clone();
+        handles.push(tokio::spawn(async move {
+            let _ = sem.acquire().await;
+            entry
+                .download_file(directory, sem)
+                .await
+                .expect("Could not download entry");
+        }));
     }
+
+    join_all(handles).await;
 
     Ok(())
 }
